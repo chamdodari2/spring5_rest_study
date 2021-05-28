2c5fbab711c527fc7848f02c50dc04f8cefaf9f8
yjkim <chitose_@naver.com>
2021-05-28 16:24:40 +0900

    삭제

--- src/main/java/spring5_rest_study/controller/RestDeleteMemberController.java
+++ src/main/java/spring5_rest_study/controller/RestDeleteMemberController.java
@@ -2,8 +2,8 @@ package spring5_rest_study.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +15,9 @@ public class RestDeleteMemberController {
 	private RemoveMemberService service;
 
 	
-	@PostMapping("/members/{id}")
-	public ResponseEntity<Object> deleteMember ( @PathVariable long id){
-		System.out.println("deleteMember > " + id );
+	@DeleteMapping("/members/{id}")
+	public ResponseEntity<Object> deleteMember(@PathVariable long id){
+	System.out.println("deleteMember > " + id);
 	return ResponseEntity.ok(service.removeMember(id));
-			
 	}
 }
