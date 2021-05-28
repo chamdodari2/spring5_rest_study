2e423ec5f46d96fe6040aac97bbadb4ced21d11d
yjkim <chitose_@naver.com>
2021-05-28 09:45:35 +0900

    초기화 및 database connection complete, sql

---
+++ src/main/java/spring5_rest_study/config/ContextRoot.java
@@ -0,0 +1,19 @@
+package spring5_rest_study.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ContextDataSource.class,ContextSqlSession.class}) //여기에 있는 설정파일(Bean) 가져온다
+@ComponentScan(basePackages = {
+//		"spring5_webmvc_mybatis_study.service"
+//		,"spring5_webmvc_mybatis_study.controller"
+//		,"spring5_webmvc_mybatis_study.mapper"
+//		,"spring5_webmvc_mybatis_study.common"
+//		,"spring5_webmvc_mybatis_study.exception"
+		})
+public class ContextRoot {
+
+}
